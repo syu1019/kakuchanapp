08f4ae7d1f5a98a4d0798d8a3fea31f917eee799
kaku <kaku981019@gmail.com>
2022-01-07 22:48:37 +0900

    不要なscriptタグ削除

--- kakuchan_app/resources/views/index.blade.php
+++ kakuchan_app/resources/views/index.blade.php
@@ -6,11 +6,7 @@
     <meta name="description" content="サイトキャプションを入力">
     <meta name="keywords" content="サイトキーワードを,で区切って入力">
     <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
-    <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-    <![endif]-->
-    <script src="sample.js"></script>
+
 </head>
 
 <body>
