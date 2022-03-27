417523be093fc28af07343c65685f10be66f0a05
角田脩 <kaku981019@gmail.com>
2022-03-27 12:59:19 +0900

    コントローラー経由でページ表示するように変更した

--- kakuchan_app/routes/web.php
+++ kakuchan_app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', function () {
-    return view('blog');
-});
+Route::get('/blog', [BlogController::class, 'display']);
 
 Route::get('/createBlog', function () {
     return view('createBlog');
