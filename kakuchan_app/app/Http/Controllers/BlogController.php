d004d8be149ddef035c0eeceb2174979361d4450
角田脩 <kaku981019@gmail.com>
2022-03-27 13:26:15 +0900

    bladeに変数渡した

--- kakuchan_app/app/Http/Controllers/BlogController.php
+++ kakuchan_app/app/Http/Controllers/BlogController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class BlogController extends Controller
 {
     public function display(){
-        $row = DB::table('article')->get();
-        return view('admin');
+        $article = DB::table('article')->get();
+        return view('blog', compact('article'));
     }
 }
