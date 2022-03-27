8bf764b3a03530034cd5a3a6e8adef77a5ae3dd8
角田脩 <kaku981019@gmail.com>
2022-03-27 13:11:15 +0900

    データとれた

--- kakuchan_app/app/Http/Controllers/BlogController.php
+++ kakuchan_app/app/Http/Controllers/BlogController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
     public function display(){
+        $row = DB::table('article')->get();
         return view('admin');
     }
 }
